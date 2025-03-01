88255361a635422fa1bcd7273df11a2f0c92cd1b
Matheuzinh0 <matheus.henriquesousa@ufpe.br>
2025-03-01 19:07:53 -0300

    uptade setup.py and LICENSE.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Leia o conteúdo do README
+readme_path = Path(__file__).parent / "README.md"
+with open(readme_path, "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="pysinaisbr",
@@ -6,9 +12,9 @@ setup(
     author="Matheus Sousa",
     author_email="matheus.henriquesousa@ufpe.br",
     description="Biblioteca brasileira para processamento de sinais e sistemas",
-    long_description=open('README.md', encoding='utf-8').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Matheuzinh0/PySinaisBR",
+    url="https://github.com/seu-usuario/PySinaisBR",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
@@ -23,6 +29,30 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 5 - Production/Stable"
     ],
     python_requires='>=3.8',
+    license="MIT",
+    license_files=["LICENSE.txt"],
+    include_package_data=True,
+    keywords=[
+        'sinais', 
+        'processamento digital',
+        'comunicações',
+        'transformada fourier',
+        'engenharia'
+    ],
+    project_urls={
+        "Documentação": "https://github.com/seu-usuario/PySinaisBR/wiki",
+        "Código Fonte": "https://github.com/seu-usuario/PySinaisBR",
+        "Tracker de Issues": "https://github.com/seu-usuario/PySinaisBR/issues"
+    },
+    options={
+        'bdist_wheel': {
+            'universal': False
+        }
+    }
 )
